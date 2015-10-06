f54e76426c97215397c6f48ac087a95987eccfba
Christina Goefron <cagoefron@gmail.com>
2015-10-06 16:02:22 -0400

    Broken stuff

---
+++ Candy Town/Assets/PlayerMovement1.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement1 : MonoBehaviour {
+
+	private float speed = 1.0f;
+	private Vector3 moveDirection;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update()
+	{
+
+	
+		//Player One Arrow Keys
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+			//moveDirection = new Vector3(10,0,0);
+
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+			//moveDirection = new Vector3(-10,0,0);
+
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			//transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+			transform.Translate(new Vector3(0,0,-speed * Time.deltaTime));
+			//moveDirection = new Vector3(0,0,10);
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			//transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+			transform.Translate(new Vector3(0,0,speed * Time.deltaTime));
+			//moveDirection = new Vector3(0,0,-10);
+		}
+
+		//Quaternion wanted_rotation = Quaternion.LookRotation(moveDirection);
+		//transform.rotation = Quaternion.RotateTowards(transform.rotation, wanted_rotation, speed * 10 * Time.deltaTime);
+	}
+	
+}
+
+
---
+++ Candy Town/Assets/PlayerMovement2.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement2 : MonoBehaviour {
+	
+	private float speed = 1.0f;
+	private Vector3 moveDirection;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update()
+	{
+
+		//Player TWO WASD
+		if(Input.GetKey(KeyCode.D))
+		{
+			//transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+			moveDirection = new Vector3(10,0,0);
+			//transform.Rotate = new Vector3(5,0,0);
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			//transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+			moveDirection = new Vector3(-10,0,0);
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			//transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+			transform.Translate(new Vector3(0,0,-speed * Time.deltaTime));
+			//moveDirection = new Vector3(0,0,10);
+		}
+		if(Input.GetKey(KeyCode.W))
+		{
+			//transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+			transform.Translate(new Vector3(0,0,speed * Time.deltaTime));
+			//moveDirection = new Vector3(0,0,-10);
+		}
+		
+		Quaternion wanted_rotation = Quaternion.LookRotation(moveDirection);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, wanted_rotation, speed * 20 * Time.deltaTime);
+	}
+	
+}
